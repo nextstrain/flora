4418a7e3a11898fea7255290a05c417eacca6b95
James Hadfield <jh22@sanger.ac.uk>
2017-10-26 10:42:58 -0700

    upload with DBdiff

--- scripts/upload.py
+++ scripts/upload.py
@@ -3,6 +3,7 @@ import os, json, sys
 import rethinkdb as r
 import argparse
 from pdb import set_trace
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', default='input_data/', help="Path to directory containing upload file, default is input_data/")
@@ -10,6 +11,7 @@ parser.add_argument('--path', default='input_data/', help="Path to directory con
 # parser.add_argument('--auth_key', default=None, help="auth_key for rethink database")
 # parser.add_argument('--local', default=False, action="store_true",  help ="connect to local instance of rethinkdb database")
 parser.add_argument('--filename', '--file', '-f', required=True, help="file to upload to rethink database")
+parser.add_argument('--change_db', default=False, action="store_true", help="perform changes to the DB (default: False)")
 
 
 def parse_sacra_json(filename):
@@ -25,27 +27,78 @@ def parse_sacra_json(filename):
         sys.exit(2)
     return dataset
 
+def checkEqual(iterator):
+    iterator = iter(iterator)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
+
+def change_db_via_json(table, sacra, pkey, do):
+    ## 1: what fields are in the json_in (and is it consistent?)\
+    sacra_consistent = checkEqual([set(x.keys()) for x in sacra])
+    print("Is sacra consistent? {}".format(sacra_consistent))
+
+    ## 2 not 2: what fields will be added to the table?
+
+    ## 2: seperate json_in into udate and upload portions
+    primary_keys_sacra = [x[pkey] for x in sacra]
+    primary_keys_db = set(table.get_all(*primary_keys_sacra).get_field(pkey).run())
+    updates = filter(lambda x: x[pkey] in primary_keys_db, sacra)
+    inserts = filter(lambda x: x[pkey] not in primary_keys_db, sacra)
+
+    ## 3: perform inserts
+    print("Inserting (creating) {} rows".format(len(inserts)))
+    for i in inserts:
+        print("\t{}".format(i[pkey]))
+    if do:
+        table.insert(inserts).run()
+
+    ## 4: for updates:
+    updates_db = table.get_all(*[x[pkey] for x in updates]).coerce_to('array').run()
+    updates_db_dict = {x[pkey]:x for x in updates_db}
+    updates_sacra_dict = {x[pkey]:x for x in updates}
+
+    ## 4a: are rows unchanged (then no-op)
+    identicals = filter(lambda x: updates_db_dict[x] == updates_sacra_dict[x], updates_sacra_dict.keys())
+    print("Skipping {} rows as they are identical in the input & the DB".format(len(identicals)))
+    for i in identicals:
+        print("\t{}: {}".format(pkey, i))
+    for i in identicals:
+        updates_sacra_dict.pop(i, None)
+        updates_db_dict.pop(i, None)
+
+    ## 4b: for other rows, what fields are changing (op)
+    updates_to_do = defaultdict(dict)
+    print("Updating (modifying) {} rows".format(len(updates_sacra_dict.keys())))
+    for row_key in updates_sacra_dict.keys():
+        print("\t{}: {}".format(pkey, row_key))
+        for (k, v) in updates_sacra_dict[row_key].iteritems():
+            if v == updates_db_dict[row_key][k]: continue
+            if k in updates_db_dict[row_key]:
+                print("\t\t{}: {} -> {}".format(k, updates_db_dict[row_key][k], v))
+            else:
+                print("\t\t{}: (not present) -> {}".format(k, v))
+            updates_to_do[row_key][k] = v
+
+    if do:
+        for row_key in updates_to_do:
+            table.get(row_key).update(updates_to_do[row_key]).run()
+
+
+
 if __name__=="__main__":
     args = parser.parse_args()
     sacra = parse_sacra_json(os.path.join(args.path, args.filename))
 
-    r.connect("localhost", 28015).repl()
+    conn = r.connect("localhost", 28015).repl()
     dbname = "test"
 
 
     if "sequences" in sacra:
-        primary_keys = [x["accession"] for x in sacra["sequences"]]
-        primary_keys_in_db = r.db(dbname).table("sequences").get_all(primary_keys).run()
-
-        # set_trace()
-        # ensure all unique
-
-        print("uploading {} sequences".format(len(sacra["sequences"])))
-        # I believe inserting is only for new rows? what happens if they have key colissions?
-        # Do we need to check whats new (insert) and what exists (append)
-
-        r.db(dbname).table("sequences").insert(sacra["sequences"]).run()
-
-    if "viruses" in sacra:
-        print("uploading {} pathogens".format(len(sacra["viruses"])))
-        r.db(dbname).table("pathogens").insert(sacra["viruses"]).run()
+        print("TABLE: sequences")
+        change_db_via_json(r.db(dbname).table("sequences"), sacra["sequences"], "accession", args.change_db)
+    if "pathogens" in sacra:
+        print("TABLE: pathogens")
+        change_db_via_json(r.db(dbname).table("pathogens"), sacra["pathogens"], "strain", args.change_db)
