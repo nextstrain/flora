879f358c61543a462e85f3c3a1081e6e3c13da8c
James Hadfield <jh22@sanger.ac.uk>
2017-11-15 12:01:04 -0800

    spec update: create / drop tables

--- scripts/createTables.py
+++ scripts/createTables.py
@@ -1,27 +1,25 @@
+from __future__ import print_function, division
 import rethinkdb as r
+import os, sys
+from utils.tables_primary_keys import tables_primary_keys
+import logging
 
 # https://www.rethinkdb.com/docs/guide/python/
-
 r.connect("localhost", 28015).repl()
-
+dbname = "test"
 # we assume that the "test" database is present (it is by default)
 # in reality, this will be the pathogen name, e.g. zika / cholera
-dbname = "test"
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s')
+logger = logging.getLogger(__name__)
 
 # create the tables
-for table_name, primary_key in {
-    "pathogens": "strain",
-    "sequences": "accession",
-    "geo": "name",
-    "access": "entity",
-    "authors": "author"
-    }.iteritems():
+for table_name, primary_key in tables_primary_keys.iteritems():
     r.db(dbname).table_create(table_name, primary_key=primary_key).run()
 
+logger.info("Created {} tables in the database \"{}\"".format(len(tables_primary_keys.keys()), dbname))
 
-## note, if you want to remove tables:
-# r.db(dbname).table_drop("pathogens").run()
-# r.db(dbname).table_drop("sequences").run()
-# r.db(dbname).table_drop("geo").run()
-# r.db(dbname).table_drop("access").run()
-# r.db(dbname).table_drop("authors").run()
+delete = False
+if delete:
+    for table_name, primary_key in tables_primary_keys.iteritems():
+        r.db(dbname).table_drop(table_name).run()
---
+++ scripts/utils/tables_primary_keys.py
@@ -0,0 +1,7 @@
+tables_primary_keys = {
+    "dbinfo": "pathogen",
+    "strains": "strain_id",
+    "samples": "sample_id",
+    "sequences": "sequence_id",
+    "attributions": "attribution_id"
+}
