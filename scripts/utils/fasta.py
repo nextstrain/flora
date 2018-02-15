160cb0c5c57451680e271bcda3805d5f3bae1001
James Hadfield <jh22@sanger.ac.uk>
2018-02-15 13:47:44 -0800

    fixed zika fasta header

--- scripts/utils/fasta.py
+++ scripts/utils/fasta.py
@@ -1,4 +1,4 @@
 headers = {
     "default": ['strain_name', 'sample_name', 'collection_date', 'country', 'division', 'subdivision', 'accession', 'accession_url', 'authors', 'attribution_journal', 'attribution_title', 'attribution_url'],
-    "zika": ['strain_name', 'empty', 'accession', 'collection_date', 'region', 'country', 'division', 'empty,' 'db', 'empty', 'authors', 'accession_url', 'attribution_title', 'attribution_journal', 'attribution_url']
+    "zika": ['strain_name', 'empty', 'accession', 'collection_date', 'region', 'country', 'division', 'empty', 'db', 'empty', 'authors', 'accession_url', 'attribution_title', 'attribution_journal', 'attribution_url']
 }
