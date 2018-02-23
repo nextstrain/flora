d85b878d8bdf9c1bac926fa1ca060ee401a0060c
Barney Potter <barney.potter.24@gmail.com>
2018-02-23 14:31:01 -0800

    Update fasta headers for zika builds.

--- scripts/utils/fasta.py
+++ scripts/utils/fasta.py
@@ -1,4 +1,20 @@
 headers = {
     "default": ['strain_name', 'sample_name', 'collection_date', 'country', 'division', 'subdivision', 'accession', 'accession_url', 'authors', 'attribution_journal', 'attribution_title', 'attribution_url'],
-    "zika": ['strain_name', 'empty', 'accession', 'collection_date', 'region', 'country', 'division', 'empty', 'db', 'empty', 'authors', 'accession_url', 'attribution_title', 'attribution_journal', 'attribution_url']
+    "zika": [
+                'strain_name',
+                'virus',
+                'accession',
+                'collection_date',
+                'empty', # region
+                'country',
+                'division',
+                'location',
+                'empty', # genbank field
+                'empty', # genome
+                'authors',
+                'accession_url',
+                'publication_name',
+                'journal',
+                'attribution_url'
+            ]
 }
