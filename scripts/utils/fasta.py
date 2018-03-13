22f9a0e25b58ccafc904c2d348dec28983ea5d1c
James Hadfield <jh22@sanger.ac.uk>
2018-03-13 14:14:24 -0700

    fluA FASTA headers

--- scripts/utils/fasta.py
+++ scripts/utils/fasta.py
@@ -1,20 +1,32 @@
 headers = {
     "default": ['strain_name', 'sample_name', 'collection_date', 'country', 'division', 'subdivision', 'accession', 'accession_url', 'authors', 'attribution_journal', 'attribution_title', 'attribution_url'],
     "zika": [
-                'strain_name',
-                'virus',
-                'accession',
-                'collection_date',
-                'empty', # region
-                'country',
-                'division',
-                'location',
-                'empty', # genbank field
-                'empty', # genome
-                'authors',
-                'accession_url',
-                'publication_name',
-                'journal',
-                'attribution_url'
-            ]
+        'strain_name',
+        'virus',
+        'accession',
+        'collection_date',
+        'empty', # region
+        'country',
+        'division',
+        'location',
+        'empty', # genbank field
+        'empty', # genome
+        'authors',
+        'accession_url',
+        'publication_name',
+        'journal',
+        'attribution_url'
+    ],
+    "fluA": [
+        'strain_name',
+        'collection_date',
+        'country',
+        'region',
+        'segment',
+        'type',
+        'ha_type',
+        'na_type',
+        'host_species',
+        'accession'
+    ],
 }
